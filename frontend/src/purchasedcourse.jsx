881649972cf02578b5d8676b9f8f496cf0017177
SAKSHAMGUPTA123456 <sakshamgupta0155.in>
2025-08-18 23:51:32 +0530

    s

--- frontend/src/purchasedcourse.jsx
+++ frontend/src/purchasedcourse.jsx
@@ -50,7 +50,7 @@ export const Alreadypurchased = () => {
       const purchasedDetails = wholeoldarray.filter(course =>
         oldarray.some(p => p.courseId === course._id)
       );
-      if(purchasedDetails!=0){
+      if(purchasedDetails.length!=0){
       newa(purchasedDetails);
       }
     }
